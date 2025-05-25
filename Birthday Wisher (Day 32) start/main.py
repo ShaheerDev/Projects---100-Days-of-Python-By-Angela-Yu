249fe8b1552018a38ee11dd73b65b1ed79d84d1c
ShaheerDev <mohammadshaheermirza@gmail.com>
2025-05-25 19:07:30 +0500

    Add files via upload

---
+++ Birthday Wisher (Day 32) start/main.py
@@ -0,0 +1,25 @@
+
+quotes_list = []
+import datetime as dt
+import random
+import smtplib
+
+now = dt.datetime.now()
+current_day = now.weekday()
+if current_day == 6:
+    with open("quotes.txt") as data:
+        quotes = data.readlines()
+        for quote in quotes:
+            quotes_list.append(quote)
+        random_quote = random.choice(quotes_list)
+
+my_email = "emaildummy130@gmail.com"
+password = "iehkvgyjcafbgtxm"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="toobakamran2012@gmail.com",
+        msg=f"Subject: Monday Quotes \n\n {random_quote}")
